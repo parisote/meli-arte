9f09313842deb88fd5cc2d03f6fe849e35f77ebb
tomidamico <tomidamico@hotmail.com>
2021-03-28 19:44:32 -0300

    Iniciando Index

---
+++ WebPNT/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("WebPNT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebPNT")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("d6e2b93e-e45e-4e2a-92af-fe319dd2c2c8")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
