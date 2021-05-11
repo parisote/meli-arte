87662e083f5b833c1953741af0422a9a95328c13
Carolina Romero <carolina.romero@sondeos.com.ar>
2021-05-10 21:53:09 -0300

    Counter isnt working but its pretty

--- meli/meli/component.js
+++ meli/meli/component.js
@@ -15,6 +15,7 @@ class OCard extends HTMLElement {
     this.render();
   }
 
+
   render() {
     this.innerHTML = `
       <div class="card mb-3" style="max-height:32em">
@@ -31,6 +32,11 @@ class OCard extends HTMLElement {
         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
         </svg>
         </a>
+        <div class="btn-group-vertical btn-group-sm" role="group" aria-label="Basic outlined example">
+          <button type="button" class="btn btn-outline-primary"><i class="tiny material-icons">keyboard_arrow_up</i></button>
+          <button type="button" class="btn btn-outline-primary">2</button>
+          <button type="button" class="btn btn-outline-primary"><i class="tiny material-icons">keyboard_arrow_down</i></button>
+        </div>
       </div>
       </div>
       </div>
